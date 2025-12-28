cf251383b8f1a6b5e52dc332c24aa9610ff96c05
HarshalGunjalOp <hrshlgunjal3@gmail.com>
2025-12-28 17:32:19 +0530

    Ai coding job in inngest

--- inngest/functions.ts
+++ inngest/functions.ts
@@ -1,10 +1,18 @@
+import { Agent, openai, createAgent } from "@inngest/agent-kit"
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
 	{ id: "hello-world" },
 	{ event: "test/hello.world" },
 	async ({ event, step }) => {
-		await step.sleep("wait-a-moment", "10s");
-		return { message: `Hello ${event.data.email}!` };
+		const codeAgent = createAgent({
+			name: "code-agent",
+			system: "You are an expert next.js developer. You write readable, maintainable code. You write simple nextjs and react snippets.",
+			model: openai({ model: "gpt-5-nano" }),
+		});
+		const { output } = await codeAgent.run(
+			`Write the following snippet: ${event.data.value}`
+		);
+		return {output};
 	}
 );
